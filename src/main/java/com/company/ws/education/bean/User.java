3f2dfeaa967519b153185658ab9f9cdee12b849c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-25 23:45:14 +0400

    User

---
+++ src/main/java/com/company/ws/education/bean/User.java
@@ -0,0 +1,29 @@
+package com.company.ws.education.bean;
+
+public class User {
+
+    private String name;
+    private String surname;
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
